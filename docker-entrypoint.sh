30e61ef3a29b516fbabd4e46a50362c0a9310484
James <james@mercstudio.com>
2017-09-26 01:54:50 +0800

    install node sass for linux

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,6 +3,7 @@ echo "GOPATH $GOPATH"
 # cd $GOPATH/src/github.com/markbates/pop
 # git pull
 cd $GOPATH/src/$1
+npm rebuild node-sass
 echo "pwd: `pwd`"
 echo "buidling..."
 buffalo $2 $3 $4 $5 $6 $7
